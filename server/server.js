a25a9a72fa4933f22255ce5f9e9daa40bfdcde89
programmer-fe5 <programmer-fe5@qwertysystem.net>
2024-07-21 14:44:31 +0700

    feat: add cors middleware

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 
+import corsOption from "./config/cors.config.js";
 import connectToDB from "./db/connect.db.js";
+import credentials from "./middlewares/credentials.middleware.js";
 import { authRoutes, messageRoutes, userRoutes } from "./routes/index.js";
 
 dotenv.config();
@@ -10,8 +14,10 @@ const app = express();
 const port = process.env.PORT;
 
 // Middleware
+app.use(credentials);
+app.use(cors(corsOption));
 app.use(express.json()); // To parse the incoming request with JSON payloads
-// app.use(cookieParser());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
