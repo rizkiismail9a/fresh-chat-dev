1de84b6960c827058a9c4dbe84c4384a34100366
programmer-fe5 <programmer-fe5@qwertysystem.net>
2024-07-23 17:55:26 +0700

    fix: wrong height skeleton

--- client/src/components/Skeleton/ConversationSkeleton.jsx
+++ client/src/components/Skeleton/ConversationSkeleton.jsx
@@ -1,6 +1,6 @@
 const MessageSkeleton = () => {
   return (
-    <>
+    <div className="p-4 flex-1 flex flex-col gap-2 overflow-auto">
       <div className="flex gap-3 items-center">
         <div className="skeleton w-10 h-10 rounded-full shrink-0 glass"></div>
         <div className="flex flex-col gap-1">
@@ -13,7 +13,7 @@ const MessageSkeleton = () => {
           <div className="skeleton h-12 w-60 glass"></div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 export default MessageSkeleton;
